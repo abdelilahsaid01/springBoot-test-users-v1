b87f4123c61adee3c81d826689079adc12128fc3
abdelilahsaid01 <abdelilahsaid01@gmail.com>
2024-10-20 13:00:11 +0100

    fix integration test issue

--- src/test/java/com/example/springboottest/UserControllerTest.java
+++ src/test/java/com/example/springboottest/UserControllerTest.java
@@ -11,11 +11,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
